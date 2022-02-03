c4646c0ac2baf6e358b724acb9195a197c0fba8e
dominichayesferen <ferenosdev@outlook.com>
2022-02-03 20:01:42 +0000

    Added a conceptual updated fork of Start Page for lols

---
+++ start-page-redesign/engines/base.js
@@ -0,0 +1,108 @@
+/*	DUCKDUCKGO	
+	----------------------------------------------------- */
+
+eng.duckduckgo = {
+	logo: "engines/duckduckgo.png",
+    enginestr: "DuckDuckGo",
+	uri: "https://www.ddg.co/?q=%query%"
+};
+
+
+/*	ECOSIA
+	----------------------------------------------------- */
+
+eng.ecosia = {
+	logo: "engines/ecosia.png",
+    enginestr: "Ecosia",
+	uri: "https://www.ecosia.org/search?q=%query%"
+};
+
+
+/*	GOOGLE
+	----------------------------------------------------- */
+
+eng.google = {
+	logo: "engines/google.png",
+    enginestr: "Google",
+	uri: "http://www.google.com/search?q=%query%"
+};
+
+
+/*	BING
+	----------------------------------------------------- */
+
+eng.bing = {
+	logo: "engines/bing.png",
+    enginestr: "Microsoft Bing",
+	uri: "https://www.bing.com/search?q=%query%"
+};
+
+
+/*	START PAGE
+	----------------------------------------------------- */
+
+eng.startpage = {
+	logo: "engines/startpage.png",
+    enginestr: "Startpage",
+	uri: "https://www.startpage.com/do/search?q=%query%"
+};
+
+
+/*	YOUTUBE
+	----------------------------------------------------- */
+
+eng.youtube = {
+	logo: "engines/youtube.png",
+    enginestr: "YouTube",
+	uri: "http://www.youtube.com/results?search_query=%query%"
+};
+
+
+/*	QWANT
+	----------------------------------------------------- */
+
+eng.qwant = {
+	logo: "engines/qwant.png",
+    enginestr: "Qwant",
+	uri: "https://www.qwant.com/?q=%query%"
+};
+
+
+/*	YAHOO
+	----------------------------------------------------- */
+
+eng.yahoo = {
+	logo: "engines/yahoo.png",
+    enginestr: "Yahoo",
+	uri: "https://search.yahoo.com/search?p=%query%"
+};
+
+
+/*	BRAVE	
+	----------------------------------------------------- */
+
+eng.brave = {
+	logo: "engines/brave.png",
+    enginestr: "Brave",
+	uri: "https://search.brave.com/search?q=%query%"
+};
+
+
+/*	BAIDU
+	----------------------------------------------------- */
+
+eng.baidu = {
+	logo: "engines/baidu.png",
+    enginestr: "百度搜索",
+	uri: "https://www.baidu.com/s?wd=%query%"
+};
+
+
+/*	NICOVIDEO
+	----------------------------------------------------- */
+
+eng.nicovideo = {
+	logo: "engines/nicovideo.png",
+    enginestr: "ニコニコ動画",
+	uri: "https://www.nicovideo.jp/search/%query%"
+};
