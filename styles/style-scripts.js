52fad0eb96eb3e91547620770b6e819de230ae69
dominichayesferen <ferenosdev@outlook.com>
2022-02-11 23:03:04 +0000

    one note for later

--- styles/style-scripts.js
+++ styles/style-scripts.js
@@ -1,6 +1,7 @@
 var UA=navigator.userAgent;
 
 function browserOptimisations() {
+    //TODO: Do it in reverse (start with Basic, remove Basic for fully capable browsers once onload)
     if (/rv:([^\)]+)\) Gecko\/\d{8}/.test(UA) && /Firefox\/(\S+)/.test(UA)) { /*Firefox*/
         //Use Basic theme since the browser doesn't support blur
         //TODO: Find out UAs of other non-Firefox browsers that also do not support blur effects
