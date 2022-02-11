01b1fe80980651e6a7fb2242eaa7cecfb041739b
dominichayesferen <ferenosdev@outlook.com>
2022-02-11 22:42:03 +0000

    fixes

--- styles/style-scripts.js
+++ styles/style-scripts.js
@@ -1,10 +1,12 @@
+var UA=navigator.userAgent;
+
 function browserOptimisations() {
     if (/rv:([^\)]+)\) Gecko\/\d{8}/.test(UA) && /Firefox\/(\S+)/.test(UA)) { /*Firefox*/
         //Use Basic theme since the browser doesn't support blur
         //TODO: Find out UAs of other non-Firefox browsers that also do not support blur effects
         var head  = document.getElementsByTagName('head')[0];
         var link  = document.createElement('link');
-        link.id   = cssId;
+        link.id   = 'Basic';
         link.rel  = 'stylesheet';
         link.type = 'text/css';
         link.href = '/styles/basic.css';
