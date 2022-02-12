62a99a14f582fe50edc1bb709040213b4f1ddcfe
dominichayesferen <ferenosdev@outlook.com>
2022-02-12 16:07:46 +0000

    Basic -> Feren OS style is now in reverse to make it not look like an automatic downgrade of visuals

--- styles/style-scripts.js
+++ styles/style-scripts.js
@@ -1,17 +1,11 @@
 var UA=navigator.userAgent;
 
 function browserOptimisations() {
-    //TODO: Do it in reverse (start with Basic, remove Basic for fully capable browsers once onload)
     if (/rv:([^\)]+)\) Gecko\/\d{8}/.test(UA) && /Firefox\/(\S+)/.test(UA)) { /*Firefox*/
         //Use Basic theme since the browser doesn't support blur
         //TODO: Find out UAs of other non-Firefox browsers that also do not support blur effects
+    } else {
         var head  = document.getElementsByTagName('head')[0];
-        var link  = document.createElement('link');
-        link.id   = 'Basic';
-        link.rel  = 'stylesheet';
-        link.type = 'text/css';
-        link.href = '/styles/basic.css';
-        link.media = 'all';
-        head.appendChild(link);
+        head.removeChild(document.getElementById("basiccss"));
     }
 }
