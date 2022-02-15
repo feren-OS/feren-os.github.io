b59bd29401208f1f3f13b396481d8165ed53882a
dominichayesferen <ferenosdev@outlook.com>
2022-02-15 20:21:56 +0000

    ...

--- styles/style-scripts.js
+++ styles/style-scripts.js
@@ -1,7 +1,7 @@
 var UA=navigator.userAgent;
 
 function browserOptimisations() {
-    if (/rv:([^\)]+)\) Gecko\/\d{8}/.test(UA) && /Firefox\/(\S+)/.test(UA)) || (/rv:([^\)]+)\) AppleWebKit\/\d{8}/.test(UA) && /Safari\/(\S+)/.test(UA)) { /*Firefox*/
+    if ((/rv:([^\)]+)\) Gecko\/\d{8}/.test(UA) && /Firefox\/(\S+)/.test(UA)) || (/rv:([^\)]+)\) AppleWebKit\/\d{8}/.test(UA) && /Safari\/(\S+)/.test(UA))) { /*Firefox*/
         //Use Basic theme since the browser doesn't support blur
         //TODO: Find out UAs of other non-Firefox browsers that also do not support blur effects
     } else {
