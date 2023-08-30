4292188abe5654700d0ba1cb6e6c82e9111bcf45
ajstrongdev <adamjsalt@icloud.com>
2023-08-30 13:21:33 +0100

    A blank slate

--- scripts/browserredirs.js
+++
@@ -1,7 +0,0 @@
-function browserRedirs() {
-    if (window.document.documentMode) { /*I.E.*/
-        window.location = "https://feren-os.github.io/goodbye-browser/ie";
-        return true
-    }
-    return false
-}
