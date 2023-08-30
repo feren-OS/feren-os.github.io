c3b0173e8103b3956d24e657e8926c8164d1c604
ajstrongdev <adamjsalt@icloud.com>
2023-08-30 13:25:14 +0100

    Initialise repo

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'template': '#ffffff',
+      },
+      backgroundImage: {
+        
+      }
+    },
+  },
+  plugins: [],
+}
