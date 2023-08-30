8b920f2345e388b5cd9be26ac5c9002e176ab6e9
ajstrongdev <adamjsalt@icloud.com>
2023-08-30 14:00:05 +0100

    add: generic landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'template': '#ffffff',
+        'site-100': '#ffffff',
+        'site-200': '#F8F8F8',
+        'site-300': '#F5F5F5',
+        'site-400': '#F0F0F0',
+        'feren-blue': '#0086ED',
+        'off-black': '#101010',
       },
       backgroundImage: {
         
